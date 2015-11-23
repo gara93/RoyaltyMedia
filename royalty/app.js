ce6a295d473d4969594757411c45461ac0c01dba
gara93 <gara9315@gmail.com>
2015-11-23 15:28:24 -0600

    comit
    conexion con base de datos.

--- royalty/app.js
+++ royalty/app.js
@@ -9,6 +9,9 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+function getApp(db)
+{
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -25,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+console.log("Conected To DB" + db.databaseName);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -55,6 +59,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+return app;
+}
 
 
-module.exports = app;
+module.exports = getApp;
