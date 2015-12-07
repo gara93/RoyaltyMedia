4919d59859a0a8df1cf79d3635fceb99569467c8
gara93 <gara9315@gmail.com>
2015-12-07 17:57:59 -0600

    Imagenes y css
    Css e imagenes que se utilizaran en el proyecto.

---
+++ royalty/public/css/m.js
@@ -0,0 +1,29 @@
+function updateBackground() {
+screenWidth = $(window).width();
+screenHeight = $(window).height();
+var bg = jQuery("#bg");
+
+// Proporcion horizontal/vertical. En este caso la imagen es cuadrada
+ratio = 1;
+
+if (screenWidth/screenHeight > ratio) {
+$(bg).height("auto");
+$(bg).width("100%");
+} else {
+$(bg).width("auto");
+$(bg).height("100%");
+}
+
+// Si a la imagen le sobra anchura, la centramos a mano
+if ($(bg).width() > 0) {
+$(bg).css('left', (screenWidth - $(bg).width()) / 2);
+}
+}
+$(document).ready(function() {
+// Actualizamos el fondo al cargar la pagina
+updateBackground();
+$(window).bind("resize", function() {
+// Y tambien cada vez que se redimensione el navegador
+updateBackground();
+});
+});
