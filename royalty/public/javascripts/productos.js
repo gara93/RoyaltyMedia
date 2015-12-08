09e5bfd67ad8676b5dc86ce0d275cd2158275d30
gara93 <gara9315@gmail.com>
2015-12-08 17:02:53 -0600

    Update productos.js

--- royalty/public/javascripts/productos.js
+++ royalty/public/javascripts/productos.js
@@ -1,32 +1,4 @@
-console.log("Javascript Loaded");
-
-
-function btnNewStory_onclicked(e){
-    e.preventDefault();
-    e.stopPropagation();
-    //Primer obtener los datos del formulario
-    var formValuesArray = $("#newbacklog_form").serializeArray();
-    var formObject = {};
-    for(var i = 0; i< formValuesArray.length;i++){
-        formObject[formValuesArray[i].name] = formValuesArray[i].value;
-    }
-    $.post(
-        "api/addtobacklog",
-        formObject,
-        function(data,sucess,xhr){
-            if(data.resultado.ok){
-                $("#newbacklog_form").get()[0].reset();
-                alert("Historia Ingresada!");
-
-            }else{
-                alert("Error al Insertar!");
-            }
-        },
-        "json"
-    ).fail(function(xhr,failtxt,data){
-        alert("Error al Insertar!");
-    });
-}
+console.log("Javascript Loaconsole.log("Javascript Loaded");
 
 
 
@@ -83,18 +55,62 @@ $("#perfil").on("pagecreate",function(e){
                 "success":function(jsonDoc,status,jqXHR){
                     console.log(jsonDoc);
                     var htmlstr = "";
+                    var total = 0;
                     for(var i = 0 ; i < jsonDoc.producto.length; i++){
                       htmlstr +=  '<h2><li>'+jsonDoc.producto[i].Descripcion+'</li></h2>'
                         for(var j=0 ; j< 2; j++){
                           var des=jsonDoc.producto[i].items[j].Descrip;
                           var precio=jsonDoc.producto[i].items[j].Precio;
+                          total += jsonDoc.producto[i].items[j].Precio;
+
                         htmlstr +=  jsonDoc.producto[i].items[j].Descrip+'</br>';
                         htmlstr +=  'Lps.'+jsonDoc.producto[i].items[j].Precio+'</br>'
                         htmlstr +=  '<img src="/img/'+jsonDoc.producto[i].items[j].Img+'" width="15%" ></br>'
-                        htmlstr +=  '<a href="/api/insertarCarrito/'+des+'" id="insertar"   data-role="button" data-ajax="false" >Comprar</a></br></br>'
+                        htmlstr +=  '<a href="/api/insertarCarrito/'+des+'/'+precio+'" id="insertar"   data-role="button" data-ajax="false" >Comprar</a></br></br>'
                       }
                     }
+                      htmlstr += total;
+
                     librosList.html(htmlstr).listview("refresh");
+                },
+                "error":function(jqXHR,status, errorMsg){
+                    console.log(errorMsg);
+                }
+
+            }
+        );
+});
+
+
+
+
+
+
+$("#carro").on("pagecreate",function(e){
+    var libroscarros = $("#libroscarros");
+    $.ajax("/api/obtenerCarrito",
+            {
+                "method":"GET",
+                "data":{},
+                "dataType":"json",
+                "success":function(jsonDoc,status,jqXHR){
+                    console.log(jsonDoc);
+                    var htmlstr = "";
+                    var totalCompra = 0;
+                    for(var i = 0 ; i < jsonDoc.usuario.length; i++){
+                      htmlstr +=  '<h2><li>'+jsonDoc.usuario[i].Usuario+'</li></h2>'
+                        for(var j=0 ; j< jsonDoc.usuario[i].Carrito.length; j++){
+                        totalCompra += JSON.parse(jsonDoc.usuario[i].Carrito[j].Precio);
+                        htmlstr +=  'Producto. =  '+jsonDoc.usuario[i].Carrito[j].Producto+'</br>';
+
+                        htmlstr +=  'Precio. =  '+ jsonDoc.usuario[i].Carrito[j].Precio+'</br>';
+                        }
+                    }
+
+                    htmlstr += 'Total a pagar. =  '+totalCompra+'</br>';
+                    htmlstr +=  '<a href="" id="facturar"   data-role="button" data-ajax="false" >Facturar</a></br></br>'
+
+                    libroscarros.html(htmlstr).listview("refresh");
                 },
                 "error":function(jqXHR,status, errorMsg){
                     console.log(errorMsg);
